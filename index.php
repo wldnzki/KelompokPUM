4eed55736f1c9b2a257c9436f2fdb4a3a134a4a8
Ripki1310 <ripkipernando074@gmail.com>
2025-11-23 04:41:38 -0800

    edit baris skema 1 versi Ripki

--- index.php
+++ index.php
@@ -305,7 +305,7 @@ $blog = mysqli_fetch_all($qBlog, MYSQLI_ASSOC);
 
     <!-- HERO -->
     <div class="hero">
-        <h1>Himpunan Mahasiswa Jurusan</h1>
+        <h1>Himpunan Mahasiswa Jurusan TI</h1>
         <h2>Teknologi Informasi</h2>
         <p>Selamat datang di website resmi HMJ TI. Mari berkembang bersama melalui kolaborasi, inovasi, dan pengabdian
             di bidang teknologi informasi.</p>
